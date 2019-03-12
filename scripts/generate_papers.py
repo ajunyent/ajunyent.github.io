90993da322023d32b77239c97d3592389c1804cb
ajunyent <adria.junyent-ferre@imperial.ac.uk>
2019-03-12 12:44:50 +0000

    Added urls to Google Scholar search results

--- scripts/generate_papers.py
+++ scripts/generate_papers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import csv
+import csv, urllib
 
 f=open('citations.csv','rb')
 reader=csv.reader(f)
@@ -10,6 +10,6 @@ for row in reader:
 	if not(any==row[6]):
 		any=row[6]
 		print '= '+any
-	print ': {'+row[1]+'} in '+row[2]+' by '+row[0]
+	print ': {[http://scholar.google.com/scholar?'+urllib.urlencode({'q' : row[1]})+' '+row[1]+']} in '+row[2]+' by '+row[0]
 
 f.close()
